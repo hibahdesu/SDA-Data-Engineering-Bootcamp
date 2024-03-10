4f4d7a52aedb649ff2973459cdc06f9474a68c72
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2024-03-10 17:52:56 +0300

    Add files via upload

---
+++ week 5/lecture1/lecture/docker-compose-install.sh
@@ -0,0 +1,12 @@
+# docker-compose 
+# https://docs.docker.com/compose/install/
+sudo apt-get update
+sudo apt install curl
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker -v 
+docker-compose -v
